36e4c093cd91cda7cb442072b3bf733100dcdb57
dityafaizal <dityafaizal99@gmail.com>
2021-10-11 15:57:16 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Footer
+  },
+  mounted() {
+    this.getApi();
+  },
+  methods: {
+    async getApi() {
+        try {
+          await this.$store.dispatch('getDataApi')
+    
+        } catch (error) {
+            console.log(error)
+        }
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+$base-color: #c6538c;
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+  background: green;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+.footer {
+  width: 100%;
+  height: 80px;
+  background: green;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" class="m-auto" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="text-secondary">data dari state {{ countStore }}</div>
+
+    <div v-if="getStateApi" class="text-primary">data dari state API {{ getStateApi.data.hourly_units.time }}</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      countStore: null
+    }
+  },
+  mounted() {
+    const dataPay = 10
+
+    this.countStore = this.$store.state.count
+  },
+  computed: {
+    getStateApi() {
+        return this.$store.state.stateApi
+    }
+  }
+}
+</script>
